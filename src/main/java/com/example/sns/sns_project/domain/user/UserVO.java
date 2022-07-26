b03fdc4f01e5fe525a8115907a51f5d942ec1b66
nayeon <kelly565033@gmail.com>
2022-07-26 16:51:49 +0900

    16:49NAYEON

---
+++ src/main/java/com/example/sns/sns_project/domain/user/UserVO.java
@@ -0,0 +1,16 @@
+package com.example.sns.sns_project.domain.user;
+
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
+public class UserVO {
+
+    private int id;
+//    private String userId, userPw, name, email;
+//    private Timestamp created_at;
+
+    @Column(name="userId", nullable = false)
+    private String userId;
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2022-07-26
+  Time: 오후 4:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>header</title>
+</head>
+<body>
+  <h1>header</h1>
+
+</body>
+</html>

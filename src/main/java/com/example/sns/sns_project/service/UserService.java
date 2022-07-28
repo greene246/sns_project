35eeac5b1a9ae4b255e28c5c5109b6b741821bc8
Administrator <sksk7412@gmail.com>
2022-07-28 15:26:14 +0900

    0728-15:26

--- src/main/java/com/example/sns/sns_project/service/UserService.java
+++ src/main/java/com/example/sns/sns_project/service/UserService.java
@@ -1,53 +1,30 @@
-package com.example.sns.sns_project.service;
+package com.example.sns.sns_project.Service;
 
-import com.example.sns.sns_project.domain.UserRepository;
-import com.example.sns.sns_project.domain.UserRequestDto;
-import com.example.sns.sns_project.domain.UserVO;
+import com.example.sns.sns_project.domain.user.UserRepository;
+import com.example.sns.sns_project.domain.user.UserRequestDto;
+import com.example.sns.sns_project.domain.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class UserService {
+
     @Autowired
-    private UserRepository userRepository;
+        private UserRepository userRepository;
 
-    // CRUD
-    // 1. create
-
-    public UserVO createUser(UserRequestDto userRequestDto){
-        //   UserVO user = new UserVO(userRequestDto);
-        return userRepository.save(new UserVO(userRequestDto));
-    }
-    // 2. read
-    public UserVO readUser(UserRequestDto userRequestDto){
-
-        UserVO user = userRepository.findById(userRequestDto.getId()).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 사용자입니다.")
+    public UserVO readUser(String id ) {
+        UserVO user = userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("dd")
         );
-
-        if(user != null)
-            return user;
-
-        return null;
-
+        return user;
     }
-    // 3. update
-    @Transactional // <- 조회된 객체를 업데이트
-    public void updateUser(UserRequestDto userRequestDto){
-        //     UserVO user = readUser(userRequestDto); //아이디만 동일
-        //    user.update(userRequestDto);
 
-        UserVO user = userRepository.findById(userRequestDto.getId()).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는")
-        );
-        user.update(userRequestDto);
 
-    }
-    // 4. delete
-    public void deleteUser(UserRequestDto userRequestDto){
-        userRepository.deleteById(userRequestDto.getId());
-    }
+//    public List<UserVO> readUserAll(UserRequestDto userRequestDto){
+//        return userRepository.logCheck(userRequestDto.getUser_id(),userRequestDto.getUser_pw());
+//    }
+
+
 }

ab1db7da1b395d1f5bf01ad2ff8a4bf2c016bedc
Joohyeon <wngusl1380@gmailcom>
2022-07-29 15:10:06 +0900

    7-29

--- src/main/resources/static/script/user.js
+++ src/main/resources/static/script/user.js
@@ -4,18 +4,15 @@ $('.user_id').change(e => {
     const requestData = {
         "user_id" : $('.user_id').val()
     }
-    console.log(requestData)
     $.ajax({
         url: "/getUser",
         type: "POST",
         data : JSON.stringify(requestData),
         contentType: "application/json"
     }).success(result => {
-        console.log(result + "##");
         $('#msg_err').show();
         $('#msg_ok').hide();
     }).fail(error => {
-        console.log(error.responseText +"**");
         $('#msg_ok').show();
         $('#msg_err').hide();
     })

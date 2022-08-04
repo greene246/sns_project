04f4ea2cb5443232c65605b76cda41ff55c1b213
Administrator <sksk7412@gmail.com>
2022-08-04 14:26:26 +0900

    0804-14:21

--- src/main/java/com/example/sns/sns_project/controller/UserController.java
+++ src/main/java/com/example/sns/sns_project/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.sns.sns_project.controller;
 import com.example.sns.sns_project.domain.UserVO;
 import com.example.sns.sns_project.domain.UserRequestDto;
 
+import com.example.sns.sns_project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;

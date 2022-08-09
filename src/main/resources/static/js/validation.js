761fea37d7f5fd05efbc3e49f36b111c858ddbf3
greene246 <greene246@naver.com>
2022-08-09 12:09:57 +0900

    merge with yeonjoo

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -110,44 +110,6 @@ function scrollAble() {
 }
 
 
-$(document).ready(function () {
-
-    $('.box').each(function () {
-        let content = $(this).children('.content');
-        let content_txt = content.text();
-        let content_txt_short = content_txt.substring(0, 48) + "...";
-        let btn_more = $('<a href="javascript:void(0)" class="more">더보기</a>');
-
-        $(this).append(btn_more);
-
-        if (content_txt.length >= 48) {
-            content.html(content_txt_short)
-
-        } else {
-            btn_more.hide()
-        }
-
-        btn_more.click(toggle_content);
-        // 아래 bind가 안 되는 이유는??
-        // btn_more.bind('click',toggle_content);
-
-        function toggle_content() {
-            if ($(this).hasClass('short')) {
-                // 접기 상태
-                $(this).html('더보기');
-                content.html(content_txt_short)
-                $(this).removeClass('short');
-            } else {
-                // 더보기 상태
-                $(this).html('접기');
-                content.html(content_txt);
-                $(this).addClass('short');
-
-            }
-        }
-    });
-});
-
 // 버튼 클릭시 나의 아이디 받아오기
 function who_am_i(log) {
     $.ajax({
